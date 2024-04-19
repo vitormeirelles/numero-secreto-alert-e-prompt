c494fae55e9bd43d4bc6b60c7856ad1c39d2541e
Vitor Meirelles <vitor.tiengo@gmail.com>
2024-04-19 15:48:20 +0100

    Mudar numero maximo para 50

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroMaximo = 1000;
+let numeroMaximo = 50;
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
